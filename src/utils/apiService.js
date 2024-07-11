b30f854eabf09154dd7f75d0550e7d83bc9ccbae
Diptya Bagus Sumantry <diptyasoemantry@gmail.com>
2024-07-11 22:02:43 +0700

    handle production

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -1,8 +1,9 @@
-import axios from 'axios'
+import axios from "axios";
 
 const apiService = axios.create({
-	baseURL: 'http://localhost:5005/',
-	timeout: 60000
-})
+  // baseURL: 'http://localhost:5005/',
+  baseURL: "https://apiklinikintan.soemantry.com/",
+  timeout: 60000,
+});
 
-export default apiService
+export default apiService;
